13fca5453a7d5e63c8fe8e5fb8c3a84e34bf1544
Michael Collins <michael.collins@gmail.com>
2021-12-03 19:17:19 -0800

    prep

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python
 
-import os.path
-day = os.path.basename(__file__).split('.')[0][-2:]
 test_input = []
-input = list((int(l.strip()) for l in open(f"./inputs/day{day}").readlines()))
-
 
 def part1(input):
     return None
@@ -14,6 +10,9 @@ def part2(input):
     return None
 
 
+import os.path
+day = os.path.basename(__file__).split('.')[0][-2:]
+input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 print(f"Day {day}")
 print("test part 1:", part1(test_input))
 print("part 1:", part1(input))
