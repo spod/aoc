0825fcd3df8a02edab48af8c2f4fa96f1a05c52a
Michael Collins <michael.collins@gmail.com>
2021-12-05 22:23:26 -0800

    5.2

--- 2021/day05.py
+++ 2021/day05.py
@@ -37,14 +37,60 @@ def part1(input):
             for x in range(x1, x2 + xstep, xstep):
                 for y in range(y1, y2 + ystep, ystep):
                     grid[x][y] = grid.get(x, {}).get(y, 0) + 1
+    # pg(grid)
     result = 0
     for xs in grid.values():
         result += len(list(filter(lambda i: i >= 2, list(xs.values()))))
     return result
 
 
+def pg(grid):
+    # this doesn't draw the grid "correctly"
+    # cartesian style with (0,0) in bottom left
+    # it just prints the backing dictionary!
+    Xmin = min(list(grid.keys()))
+    Xmax = max(list(grid.keys()))
+    Ymin = min(list(grid[Xmin].keys()))
+    Ymax = max(list(grid[Xmin].keys()))
+
+    disp = []
+    for x in range(Xmin, Xmax + 1):
+        disp.append([])
+        for y in range(Xmin, Ymax + 1):
+            if grid[x].get(y, 0) >= 1:
+                disp[x].append(f"{grid[x][y]}")
+            else:
+                disp[x].append('.')
+    for l in disp:
+        print("".join(l))
+
+
 def part2(input):
-    return None
+    # number of points where lines overlap on input
+    grid = defaultdict(dict)
+    for line in input:
+        (x1, y1, x2, y2) = parse_line(line)
+
+        xstep, ystep = 1, 1
+        if x1 > x2:
+            xstep = -1
+        if y1 > y2:
+            ystep = -1
+
+        if (x1 == x2) or (y1 == y2):
+            for x in range(x1, x2 + xstep, xstep):
+                for y in range(y1, y2 + ystep, ystep):
+                    grid[x][y] = grid.get(x, {}).get(y, 0) + 1
+        else:
+            y = y1
+            for x in range(x1, x2 + xstep, xstep):
+                grid[x][y] = grid.get(x, {}).get(y, 0) + 1
+                y = y + ystep
+
+    result = 0
+    for xs in grid.values():
+        result += len(list(filter(lambda i: i >= 2, list(xs.values()))))
+    return result
 
 
 day = os.path.basename(__file__).split('.')[0][-2:]
@@ -52,5 +98,5 @@ input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 print(f"Day {day}")
 print("test part 1:", part1(test_input))
 print("part 1:", part1(input))
-# print("test part 2:", part2(test_input))
-# print("part 2:", part2(input))
+print("test part 2:", part2(test_input))
+print("part 2:", part2(input))
