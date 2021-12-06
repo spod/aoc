15bcf72300799612b0e7126728c559f6fdb87450
Michael Collins <michael.collins@gmail.com>
2021-12-05 22:51:16 -0800

    6.1 and an eventual 6.2 brrr

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,13 +1,33 @@
 #! /usr/bin/env python
 
-test_input = []
+test_input = [
+    "3,4,3,1,2"
+]
+
+def simulate(sea, days):
+    day = 0
+    
+    while day < days:
+        day += 1
+        for fish in range(len(sea)):
+            if sea[fish] == 0:
+                sea[fish] = 6
+                sea.append(8)
+            else:
+                sea[fish] -= 1
+        # print(day, len(sea), sea)
+
+    return len(sea)
 
 def part1(input):
-    return None
+    sea = [int(x) for x in input[0].split(',')]
+    return simulate(sea, 80)
+
 
 
 def part2(input):
-    return None
+    sea = [int(x) for x in input[0].split(',')]
+    return simulate(sea, 256)
 
 
 import os.path
@@ -17,4 +37,4 @@ print(f"Day {day}")
 print("test part 1:", part1(test_input))
 print("part 1:", part1(input))
 print("test part 2:", part2(test_input))
-print("part 2:", part2(input))
+# print("part 2:", part2(input))
