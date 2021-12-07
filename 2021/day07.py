e57fed7b2b8a231b3fed3125c89c5612b93b8a40
Michael Collins <michael.collins@gmail.com>
2021-12-06 21:18:40 -0800

    7.1

--- 2021/day07.py
+++ 2021/day07.py
@@ -1,9 +1,25 @@
 #! /usr/bin/env python
 
-test_input = []
+test_input = [
+    "16,1,2,0,4,2,7,1,2,14"
+]
 
 def part1(input):
-    return None
+    crabs = [int(x) for x in input[0].split(',')]
+    minFuelCost = None
+    for pos in range(min(crabs), max(crabs) + 1):
+        cost = fuel_cost(crabs, pos)
+        if minFuelCost == None or cost < minFuelCost:
+            minFuelCost = cost
+    return minFuelCost
+
+
+def fuel_cost(crabs, target_position):
+    fuel = 0
+    for crab in crabs:
+        fuel += abs(crab - target_position)
+    return fuel
+
 
 
 def part2(input):
@@ -16,5 +32,5 @@ input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 print(f"Day {day}")
 print("test part 1:", part1(test_input))
 print("part 1:", part1(input))
-print("test part 2:", part2(test_input))
-print("part 2:", part2(input))
+# print("test part 2:", part2(test_input))
+# print("part 2:", part2(input))
