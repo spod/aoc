26064e3f3cbb3abcd1a9753ed958e591053d0547
Michael Collins <michael.collins@gmail.com>
2021-12-10 23:00:34 -0800

    minor tidy day 11

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import os.path
 test_input = [
     "5483143223",
     "2745854711",
@@ -13,17 +14,16 @@ test_input = [
     "5283751526"
 ]
 
-
-    # First, the energy level of each octopus increases by 1.
-    # Then, any octopus with an energy level greater than 9 flashes. 
-    #  This increases the energy level of all adjacent octopuses by 1,
-    #  including octopuses that are diagonally adjacent. If this causes
-    #  an octopus to have an energy level greater than 9, it also flashes.
-    #  This process continues as long as new octopuses keep having their
-    #  energy level increased beyond 9.
-    #  (An octopus can only flash at most once per step.)
-    # Finally, any octopus that flashed during this step has its energy level
-    # set to 0, as it used all of its energy to flash.
+# First, the energy level of each octopus increases by 1.
+# Then, any octopus with an energy level greater than 9 flashes.
+#  This increases the energy level of all adjacent octopuses by 1,
+#  including octopuses that are diagonally adjacent. If this causes
+#  an octopus to have an energy level greater than 9, it also flashes.
+#  This process continues as long as new octopuses keep having their
+#  energy level increased beyond 9.
+#  (An octopus can only flash at most once per step.)
+# Finally, any octopus that flashed during this step has its energy level
+# set to 0, as it used all of its energy to flash.
 
 
 def read_octopus_grid(input):
@@ -32,6 +32,7 @@ def read_octopus_grid(input):
         g.append([int(p) for p in line])
     return g
 
+
 def neighbors(r, c, max_xy):
     neighbors = []
     # all surrounding cells which exist
@@ -39,7 +40,7 @@ def neighbors(r, c, max_xy):
     for x in [-1, 0, 1]:
         for y in [-1, 0, 1]:
             if r + x >= min_xy and r + x <= max_xy and c + y >= min_xy and c + y <= max_xy and not (x == 0 and y == 0):
-                neighbors.append((r+x,c+y))
+                neighbors.append((r+x, c+y))
     return neighbors
 
 
@@ -49,16 +50,16 @@ def step_grid(g):
     for r in range(len(g)):
         for c in range(len(g)):
             g[r][c] += 1
-    
+
     # track which octopus flashed
     flashed = set()
     neighbors_flashed = set()
-    f = [ [ False for _ in range(len(g))] for _ in range(len(g))]
+    f = [[False for _ in range(len(g))] for _ in range(len(g))]
     for r in range(len(g)):
         for c in range(len(g)):
             if g[r][c] == 10:
                 g[r][c] = 0
-                flashed.add((r,c))
+                flashed.add((r, c))
                 f[r][c] = True
 
     # now do any follow up flashes based on neighbors incremented
@@ -73,42 +74,43 @@ def step_grid(g):
                 for (rr, cc) in to_bump:
                     if (rr, cc) not in flashed:
                         g[rr][cc] += 1
-                neighbors_flashed.add((r,c))
+                neighbors_flashed.add((r, c))
         for r in range(len(g)):
             for c in range(len(g)):
                 if g[r][c] >= 10:
                     g[r][c] = 0
                     if not f[r][c]:
                         f[r][c] = True
-                        flashed.add((r,c))
+                        flashed.add((r, c))
                         extra_flashed += 1
 
     # debug print surprisingly useful / pretty
     # pgf(g, flashed)
     return g, len(flashed)
 
+
 def pg(g):
     for r in g:
         print(" ".join([str(c) for c in r]))
+
 
 def pgf(g, f):
     print()
     for r in range(len(g)):
         to_print = []
         for c in range(len(g)):
-            if (r,c) in f:
+            if (r, c) in f:
                 to_print.append(f"\u001b[1m{g[r][c]}\u001b[0m")
             else:
                 to_print.append(f"{g[r][c]}")
         print(" ".join(to_print))
 
+
 def part1(input):
     steps = 100
     flashes = 0
     g = read_octopus_grid(input)
-    # pg(g)
     for s in range(steps):
-        # print(f"step {s}")
         g, f = step_grid(g)
         flashes += f
 
@@ -121,14 +123,13 @@ def part2(input):
     s = 0
     gs = len(g) * len(g[0])
     while not all_flashed:
-        # print(f"step {s}")
         g, f = step_grid(g)
         s += 1
         all_flashed = gs == f
 
     return s
 
-import os.path
+
 day = os.path.basename(__file__).split('.')[0][-2:]
 input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 print(f"Day {day}")
