081b7d37a3abe7cc57bf458af8f4b2c97d0997d4
Michael Collins <michael.collins@gmail.com>
2022-12-02 21:53:16 -0800

    Day 3 - quick tidy, black & remove unnecessary lines in part1

--- 2022/day03.py
+++ 2022/day03.py
@@ -3,7 +3,7 @@ import os.path
 
 from itertools import islice
 
-day = os.path.basename(__file__).split('.')[0][-2:]
+day = os.path.basename(__file__).split(".")[0][-2:]
 print(f"Day {day}")
 
 test_input_raw = """
@@ -19,34 +19,36 @@ test_input = list(l.strip() for l in test_input_raw.splitlines())
 input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 
 PRI = {}
-for ch in range(ord('a'), ord('z') + 1):
-    PRI[chr(ch)] = ch - ord('a') + 1
-for ch in range(ord('A'), ord('Z') + 1):
-    PRI[chr(ch)] = ch - ord('A') + 27
+for ch in range(ord("a"), ord("z") + 1):
+    PRI[chr(ch)] = ch - ord("a") + 1
+for ch in range(ord("A"), ord("Z") + 1):
+    PRI[chr(ch)] = ch - ord("A") + 27
+
 
 def part1(input):
     total = 0
     for line in input:
         if line != "":
-            (l, r) = (line[:int(len(line)/2)], line[int(len(line)/2):])
-            (sl, sr) = (set(), set())
-            sl |= set([*l])
-            sr |= set([*r])
+            (l, r) = (line[: int(len(line) / 2)], line[int(len(line) / 2) :])
+            (sl, sr) = (set([*l]), set([*r]))
             total += PRI[(sl & sr).pop()]
     return total
+
 
 print("test part 1:", part1(test_input))
 print("part 1:", part1(input))
 
 
+# From https://docs.python.org/3/library/itertools.html#itertools-recipes
 def batched(iterable, n):
     "Batch data into lists of length n. The last batch may be shorter."
     # batched('ABCDEFG', 3) --> ABC DEF G
     if n < 1:
-        raise ValueError('n must be at least one')
+        raise ValueError("n must be at least one")
     it = iter(iterable)
-    while (batch := list(islice(it, n))):
+    while batch := list(islice(it, n)):
         yield batch
+
 
 def part2(input):
     total = 0
@@ -56,6 +58,7 @@ def part2(input):
         badge = (set([*batch[0]]) & set([*batch[1]]) & set([*batch[2]])).pop()
         total += PRI[badge]
     return total
+
 
 print("test part 2:", part2(test_input))
 print("part 2:", part2(input))
