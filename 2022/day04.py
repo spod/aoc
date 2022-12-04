c00cece3db124e9e75edb1e5243295f93ce02004
Michael Collins <michael.collins@gmail.com>
2022-12-03 21:31:15 -0800

    day 4 functional style as it was quick

--- 2022/day04.py
+++ 2022/day04.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import os.path
-day = os.path.basename(__file__).split('.')[0][-2:]
+
+day = os.path.basename(__file__).split(".")[0][-2:]
 print(f"Day {day}")
 
 test_input_raw = """
@@ -15,39 +16,34 @@ test_input_raw = """
 test_input = list(l.strip() for l in test_input_raw.splitlines())
 input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 
-def parse_line(line):
-    l,r = line.split(",")
-    lmin,lmax = l.split("-")
+if "" in test_input:
+    test_input.remove("")
+
+if "" in input:
+    input.remove("")
+
+
+def pl(line):
+    l, r = line.split(",")
+    lmin, lmax = l.split("-")
     lmin, lmax = int(lmin), int(lmax)
-    rmin,rmax = r.split("-")
+    rmin, rmax = r.split("-")
     rmin, rmax = int(rmin), int(rmax)
-    return ((lmin, lmax), (rmin, rmax))
+    return (set(range(lmin, lmax + 1)), set(range(rmin, rmax + 1)))
+
 
 def part1(input):
-    count = 0
-    for line in input:
-        if line != "":
-            lrange, rrange = parse_line(line)
-            lset = set(range(lrange[0], lrange[1]+1))
-            rset = set(range(rrange[0], rrange[1]+1))
-            if lset.issubset(rset) or rset.issubset(lset):
-                # print(line)
-                count+=1
-    return count
+    f = lambda l: pl(l)[0].issubset(pl(l)[1]) or pl(l)[1].issubset(pl(l)[0])
+    return len(list(filter(f, input)))
+
 
 print("test part 1:", part1(test_input))
 print("part 1:", part1(input))
 
+
 def part2(input):
-    count = 0
-    for line in input:
-        if line != "":
-            lrange, rrange = parse_line(line)
-            lset = set(range(lrange[0], lrange[1]+1))
-            rset = set(range(rrange[0], rrange[1]+1))
-            if lset & rset:
-                # print(line)
-                count+=1
-    return count
+    return len(list(filter(lambda l: pl(l)[0] & pl(l)[1], input)))
+
+
 print("test part 2:", part2(test_input))
 print("part 2:", part2(input))
