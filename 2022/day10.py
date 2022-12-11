34c4a2cb80cad5085ed92634b1c42cd7cb799832
Michael Collins <michael.collins@gmail.com>
2022-12-10 23:51:16 -0800

    day 10 pts 1 & 2

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 import os.path
 from dataclasses import dataclass
+from itertools import islice
 
-day = os.path.basename(__file__).split('.')[0][-2:]
+day = os.path.basename(__file__).split(".")[0][-2:]
 print(f"Day {day}")
 
 test_input_raw = """
@@ -160,17 +161,31 @@ noop
 noop
 """
 
-test_input = list(l.strip() for l in test_input_raw.splitlines())
-test2_input = list(l.strip() for l in test2_input_raw.splitlines())
 
-input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
+def clean(inp):
+    if "" in inp:
+        inp.remove("")
+    return inp
+
+
+test_input = clean(list(l.strip() for l in test_input_raw.splitlines()))
+test2_input = clean(list(l.strip() for l in test2_input_raw.splitlines()))
+input = clean(list((l.strip() for l in open(f"./inputs/day{day}").readlines())))
+
 
 @dataclass
 class SimpleCpu:
     X: int
-    
+    nextX: int
+    instructions: list
+    pc: int
+    loading: bool
+    executing: tuple
+    halted: bool
+
     def __init__(self):
         self.X = 1
+        self.nextX = 1
         self.instructions = []
         self.pc = 0
         self.loading = False
@@ -178,36 +193,30 @@ class SimpleCpu:
         self.halted = False
 
     def tick(self):
-        #print(f"tick start- pc: {self.pc}, X: {self.X}, loading: {self.loading}, executing: {self.executing}, halted: {self.halted}")
-        
+        """Tick CPU"""
         if self.halted:
             return
-    
-        if not self.loading:
-        
-            self.executing = self.instructions[self.pc]
 
+        # addx V takes two cycles to complete.
+        # *After* two cycles, the X register is increased by the value V. (V can be negative.)
+        self.X = self.nextX
+
+        if not self.loading:
+            self.executing = self.instructions[self.pc]
             match self.executing[0]:
                 case "noop":
                     self.loading = False
-                
                 case "addx":
                     self.loading = True
-                    self.pc -= 1 # stall
-
+                    self.pc -= 1  # stall
         else:
             # loading addx post stall
-            self.X += self.executing[1]
+            self.nextX += self.executing[1]
             self.loading = False
             self.executing = False
-
         self.pc += 1
-
         if self.pc >= len(self.instructions):
             self.halted = True
-    
-        #print(f"tick end- pc: {self.pc}, X: {self.X}, loading: {self.loading}, executing: {self.executing}, halted: {self.halted}")
-
 
     def load(self, instructions):
         for l in instructions:
@@ -218,35 +227,59 @@ class SimpleCpu:
                 param = int(param)
                 self.instructions.append((instr, param))
 
-def part1(input):
-    if "" in input:
-        input.remove("")
 
+def part1(input):
     cpu = SimpleCpu()
     cpu.load(input)
     strengths = []
     cycle = 0
-    while cycle < 220 + 1:
+    while cycle <= 230 and not cpu.halted:
         if cycle in [20, 60, 100, 140, 180, 220]:
             strengths.append(cpu.X * cycle)
-            print(f"cycle: {cycle}, X: {cpu.X}, strength: {cpu.X * cycle}, strengths: {strengths}")
+            # print(
+            #     f"cycle: {cycle}, X: {cpu.X}, pc: {cpu.pc}, executing: {cpu.executing}, strength: {cpu.X * cycle}, strengths: {strengths}"
+            # )
         cpu.tick()
         cycle += 1
-    
     return sum(strengths)
 
+
 print("test part 1:", part1(test_input))
-print()
 print("test part 1, test 2:", part1(test2_input))
-print()
 print("part 1:", part1(input))
 
-def part2(input):
-    if "" in input:
-        input.remove("")
-    
-    
-    return None
 
-print("test part 2:", part2(test_input))
-print("part 2:", part2(input))
+def batched(iterable, n):
+    "Batch data into lists of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := list(islice(it, n)):
+        yield batch
+
+
+def part2(input):
+    cpu = SimpleCpu()
+    cpu.load(input)
+    buffer = []
+    cycle = 0
+    while cycle <= 240 and not cpu.halted:
+        cpu.tick()
+        pos = cycle % 40
+        if pos in [cpu.X - 1, cpu.X, cpu.X + 1]:
+            buffer.append("#")
+        else:
+            buffer.append(".")
+        cycle += 1
+
+    for line in batched(buffer, 40):
+        print("".join(line))
+    return buffer
+
+
+# print("test part 2:", part2(test_input))
+print("test part 2, test 2:")
+part2(test2_input)
+print("part 2:")
+part2(input)
