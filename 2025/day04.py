5392f291b2079d38ff9ba86adac11cd4ee38a1d1
Michael Collins <michael.collins@gmail.com>
2025-12-03 23:08:59 -0800

    format

--- 2025/day04.py
+++ 2025/day04.py
@@ -20,6 +20,7 @@ test_input_raw = """..@@.@@@@.
 # TODO - move Grid and Cell into a helper library? Maybe tomorrow
 type Grid = list[list[str]]
 
+
 class Cell:
     r: int
     c: int
@@ -29,30 +30,34 @@ class Cell:
         self.c = c
 
     # TODO research how to type magic methods like __add__
-    def __add__(self, other):                               # type: ignore
-        return Cell(self.r + other.r, self.c + other.c)     # type: ignore
-    
+    def __add__(self, other):  # type: ignore
+        return Cell(self.r + other.r, self.c + other.c)  # type: ignore
+
     def __str__(self) -> str:
         return f"Cell({self.r}, {self.c})"
-    
+
     def __repr__(self) -> str:
         return f"Cell({self.r}, {self.c})"
-    
+
     def valid(self, g: Grid) -> bool:
         (max_r, max_c) = g_size(g)
         return self.r >= 0 and self.c >= 0 and self.r <= max_r and self.c <= max_c
+
 
 # TODO make a typed Grid class
 def g_size(g: Grid) -> tuple[int, int]:
     #     max_r = len(grid) - 1
     # max_c = len(grid[0]) - 1
-    return (len(g) -1, len(g[0]) - 1)
+    return (len(g) - 1, len(g[0]) - 1)
+
 
 def g_cell_value(g: Grid, c: Cell) -> str:
     return g[c.r][c.c]
 
+
 def g_set_cell_value(g: Grid, c: Cell, v: str):
     g[c.r][c.c] = v
+
 
 def build_grid(input: list[str]) -> Grid:
     g: Grid = []
@@ -60,8 +65,18 @@ def build_grid(input: list[str]) -> Grid:
         g.append([c for c in line])
     return g
 
+
 def g_adj_cells(grid: Grid, c: Cell) -> list[Cell]:
-    offsets: list[Cell] = [Cell(-1, -1), Cell(-1, 0), Cell(- 1, 1), Cell(0, -1), Cell(0, 1), Cell(1, -1), Cell(1, 0), Cell(1, 1)]
+    offsets: list[Cell] = [
+        Cell(-1, -1),
+        Cell(-1, 0),
+        Cell(-1, 1),
+        Cell(0, -1),
+        Cell(0, 1),
+        Cell(1, -1),
+        Cell(1, 0),
+        Cell(1, 1),
+    ]
     results: list[Cell] = []
     for o in offsets:
         n = c + o
@@ -69,15 +84,18 @@ def g_adj_cells(grid: Grid, c: Cell) -> list[Cell]:
             results.append(n)
     return results
 
+
 def g_adj_cell_values(grid: Grid, cell: Cell) -> list[str]:
     results: list[str] = []
     for c in g_adj_cells(grid, cell):
         results.append(g_cell_value(grid, c))
     return results
 
+
 def g_print(g: Grid):
     for r in g:
         print(" ".join([str(c) for c in r]))
+
 
 test_input = list(l.strip() for l in test_input_raw.splitlines())
 input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
@@ -86,13 +104,13 @@ input = list((l.strip() for l in open(f"./inputs/day{day}").readlines()))
 def part1(input: list[str]):
     grid = build_grid(input)
     (max_r, max_c) = g_size(grid)
-    accessible_rolls : list[Cell] = []
+    accessible_rolls: list[Cell] = []
     for r in range(0, max_r + 1):
         for c in range(0, max_c + 1):
             cc = Cell(r, c)
             if g_cell_value(grid, cc) == "@":
                 neighbours = g_adj_cell_values(grid, cc)
-                if neighbours.count('@') < 4:
+                if neighbours.count("@") < 4:
                     accessible_rolls.append(cc)
     return len(accessible_rolls)
 
@@ -105,18 +123,18 @@ def part2(input: list[str]):
     grid = build_grid(input)
     (max_r, max_c) = g_size(grid)
 
-    removed_rolls : list[Cell] = []
+    removed_rolls: list[Cell] = []
     removed_last = 1
 
     while removed_last != 0:
-        accessible_rolls : list[Cell] = []
+        accessible_rolls: list[Cell] = []
         for r in range(0, max_r + 1):
             for c in range(0, max_c + 1):
                 cc = Cell(r, c)
                 if g_cell_value(grid, cc) == "@":
                     neighbours = g_adj_cell_values(grid, cc)
-                    if neighbours.count('@') < 4:
-                        g_set_cell_value(grid, cc, '.')
+                    if neighbours.count("@") < 4:
+                        g_set_cell_value(grid, cc, ".")
                         accessible_rolls.append(cc)
         removed_rolls.extend(accessible_rolls)
         removed_last = len(accessible_rolls)
